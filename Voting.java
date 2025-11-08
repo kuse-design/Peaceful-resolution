67ffb46f118723064e57bcb7ab38b1df9980ab98
Kuse_design <0luwakusejerry1@gmail.com>
2025-11-08 20:38:11 +0100

    second commit

--- Voting.java
+++ Voting.java
@@ -1,7 +1,22 @@
+
+import java.util.Scanner;
 public class Voting{
 public static void main(String[]args){
-System.out.print("Ade is a boy")
 
+int age = 0;
+        Scanner input = new Scanner(System.in);
+
+System.out.println("Enter your age : ");
+ age = input.nextInt();
+
+
+if (age  > 18 ){
+System.out.println("you are eligible to vote");
+}
+if ( age  < 18 ){
+System.out.print("you no fit vote");
+
+}
 }
 
 }
