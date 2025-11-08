df5a4d112ff39e63285b6346ae75224be8474978
Kuse_design <0luwakusejerry1@gmail.com>
2025-11-08 20:45:43 +0100

    Voting update

--- Voting.java
+++ Voting.java
@@ -3,18 +3,17 @@ import java.util.Scanner;
 public class Voting{
 public static void main(String[]args){
 
-int age = 0;
-        Scanner input = new Scanner(System.in);
+    Scanner input = new Scanner(System.in);
 
-System.out.println("Enter your age : ");
- age = input.nextInt();
+System.out.print("Enter your age : ");
+int age = input.nextInt();
 
 
 if (age  > 18 ){
 System.out.println("you are eligible to vote");
 }
 if ( age  < 18 ){
-System.out.print("you no fit vote");
+System.out.println("you are not eligible to vote");
 
 }
 }
